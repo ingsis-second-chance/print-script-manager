71f102d832d8bd6137d78d200d1787bd55601f5d
rcastromartinez <rcastromartinez@mail.austral.edu.ar>
2025-11-04 18:39:53 -0300

    auth02

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,11 @@ java {
 repositories { mavenCentral() }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	implementation("org.springframework.security:spring-security-oauth2-jose")
+
+
 	implementation("org.springframework.boot:spring-boot-starter-web")        // Web clásico
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
--- src/main/java/ingsis/printScriptManager/auth/AudienceValidator.java
+++ src/main/java/ingsis/printScriptManager/auth/AudienceValidator.java
@@ -1,4 +1,4 @@
-package auth;
+package ingsis.printScriptManager.auth;
 
 import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
@@ -6,21 +6,14 @@ import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
 import org.springframework.security.oauth2.jwt.Jwt;
 
 public class AudienceValidator implements OAuth2TokenValidator<Jwt> {
-
   private final String audience;
-
-  public AudienceValidator(String audience) {
-    this.audience = audience;
-  }
+  public AudienceValidator(String audience) { this.audience = audience; }
 
   @Override
   public OAuth2TokenValidatorResult validate(Jwt jwt) {
     OAuth2Error error = new OAuth2Error("invalid_token", "The required audience is missing", null);
-
-    if (jwt.getAudience().contains(audience)) {
-      return OAuth2TokenValidatorResult.success();
-    } else {
-      return OAuth2TokenValidatorResult.failure(error);
-    }
+    return jwt.getAudience().contains(audience)
+            ? OAuth2TokenValidatorResult.success()
+            : OAuth2TokenValidatorResult.failure(error);
   }
 }
