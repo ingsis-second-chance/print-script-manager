722e142f2bf7d85372f3fc6d4e8235eeaee94e79
rcastromartinez <rcastromartinez@mail.austral.edu.ar>
2025-11-28 18:41:17 -0300

    tests funcionan

--- src/test/java/ingsis/printScriptManager/TestSecurityConfiguration.java
+++ src/test/java/ingsis/printScriptManager/TestSecurityConfiguration.java
@@ -1,32 +1,28 @@
 package ingsis.printScriptManager;
 
-import java.time.Instant;
-import java.util.Map;
-
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.Customizer;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@Profile("test")
+@TestConfiguration
 public class TestSecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().permitAll())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-                .cors(AbstractHttpConfigurer::disable).csrf(AbstractHttpConfigurer::disable).build();
+    public SecurityFilterChain testSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {})); // no valida nada real
+
+        return http.build();
     }
 
     @Bean
-    public JwtDecoder jwtDecoder() {
-        return token -> new Jwt("mockToken", Instant.now(), Instant.now().plusSeconds(3600), Map.of("alg", "none"),
-                Map.of("sub", "mockUserId", "username", "mockUsername"));
+    public static PropertySourcesPlaceholderConfigurer testProperties() {
+        PropertySourcesPlaceholderConfigurer cfg = new PropertySourcesPlaceholderConfigurer();
+        cfg.setIgnoreUnresolvablePlaceholders(true);
+        return cfg;
     }
 }
