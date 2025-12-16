244f63cb49008a71178245d2b483b48d5a19486a
Pedrodeforonda <pdeforonda@mail.austral.edu.ar>
2025-12-16 02:23:44 -0300

    run snippet

---
+++ src/main/java/ingsis/printScriptManager/DTO/SnippetDTO.java
@@ -0,0 +1,18 @@
+package ingsis.printScriptManager.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class SnippetDTO {
+    private String snippetId;
+
+    private String version;
+
+    private List<String> inputs;
+}
